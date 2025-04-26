35a6b930caeea2ab36dc25fd8ad09992dfb37572
pedromota0 <pedrohenriquemota011@gmail.com>
2025-04-25 22:18:21 -0300

    certo

---
+++ LAB08/server.js
@@ -0,0 +1,39 @@
+var http = require('http');
+var express = require('express');
+var colors = require('colors');
+var bodyParser = require('body-parser');
+
+var app = express();
+app.use(express.static('./public'));
+app.use(bodyParser.urlencoded({ extended: false}))
+app.use(bodyParser.json())
+app.set('view engine', 'ejs')
+app.set('views', './views');
+
+var server = http.createServer(app);
+server.listen(80);
+
+console.log('Servidor rodando ...'.rainbow);
+
+app.get('/', function (requisicao, resposta){
+resposta.redirect('cadastro.html')
+})
+
+app.get('/inicio', function (requisicao, resposta){
+var nome = requisicao.query.info;
+console.log(nome);
+})
+
+app.post('/inicio', function (requisicao, resposta){
+var data = requisicao.body.data;
+console.log(data);
+})
+
+app.get('/cadastro',function (requisicao, resposta){
+var nome = requisicao.query.nome;
+var sobrenome = requisicao.query.sobrenome;
+var email = requisicao.query.nascimento;
+var time = requisicao.query.civil;
+
+resposta.render('resposta_cadastro', {nome, sobrenome, email, time})
+})
