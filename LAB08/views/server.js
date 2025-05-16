6ae32c24a931377fc09ffc07ec8a11ede9d06a80
pedromota0 <pedrohenriquemota011@gmail.com>
2025-05-16 20:45:28 -0300

    save

---
+++ LAB08/views/server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const app = express();
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'ejs');
+
+const users = [];
+
+
+app.get('/cadastra', (req, res) => {
+  res.sendFile(path.join(__dirname, 'cadastro.html'));
+});
+
+app.post('/cadastra', (req, res) => {
+  const { username, password } = req.body;
+  users.push({ username, password });
+  res.redirect('/login.html');
+});
+
+app.get('/login', (req, res) => {
+  res.sendFile(path.join(__dirname, 'login.html'));
+});
+
+app.post('/login', (req, res) => {
+  const { username, password } = req.body;
+  const user = users.find(u => u.username === username && u.password === password);
+  if (user) {
+    res.render('resposta.ejs', { status: 'Login bem-sucedido!', user: username });
+  } else {
+    res.render('resposta.ejs', { status: 'Falha no login. Usuário ou senha incorretos.', user: null });
+  }
+});
+
+app.listen(3000, () => {
+  console.log('Servidor rodando na porta 80');
+});
