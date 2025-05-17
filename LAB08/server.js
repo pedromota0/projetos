2e834c8f8bf2107a5c32bfeff360f430e1473e4c
pedromota0 <pedrohenriquemota011@gmail.com>
2025-05-16 21:07:43 -0300

    sASA

--- LAB08/server.js
+++ LAB08/server.js
@@ -22,9 +22,10 @@ app.post('/cadastra', (req, res) => {
   res.redirect('/login.html');
 });
 
-app.get('/login', (req, res) => {
-  res.sendFile(path.join(__dirname, 'login.html'));
-});
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'cadastro.html'));
+  });
+  
 
 app.post('/login', (req, res) => {
   const { username, password } = req.body;
