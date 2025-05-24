b30051b78d001b569d817eeeaa5d4102d1cc19bd
pedromota0 <pedrohenriquemota011@gmail.com>
2025-05-23 22:25:55 -0300

    save6

--- LAB10/app.js
+++ LAB10/app.js
@@ -1,17 +1,17 @@
-const express = require("express");
-const app = express();
-const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
-const routes = require("./routes/index");
+  const express = require("express");
+  const app = express();
+  const mongoose = require("mongoose");
+  const bodyParser = require("body-parser");
+  const routes = require("./routes/index");
 
-mongoose.connect("mongodb://localhost:27017/carrosdb");
+  mongoose.connect("mongodb://localhost:27017/carrosdb");
 
-app.set("view engine", "ejs");
-app.use(express.static("public"));
-app.use(bodyParser.urlencoded({ extended: true }));
+  app.set("view engine", "ejs");
+  app.use(express.static("public"));
+  app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use("/", routes);
+  app.use("/", routes);
 
-app.listen(80, () => {
-  console.log("Servidor rodando na porta 80");
-});
+  app.listen(3000, () => {
+    console.log("Servidor rodando na porta 3000");
+  });
